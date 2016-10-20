9984b0e631b3189b5ab0aa29d1a56a8eac054376
GkWangBin <gkwangbin@126.com>
2016-10-20 19:08:04 +0800

    后缀数组的实现

---
+++ 后缀数组/Instances.cpp
@@ -0,0 +1,54 @@
+﻿/*主要参考“后缀数组-处理字符串的有力工具”，论文作者是“罗穗骞”
+主要要解决的问题：
+1、最长重复出现子串；
+2、最长公共子串；
+*/
+#include"MakeSuffixArray.h"
+#include<Windows.h>
+#include<string>
+
+int main(void)
+{
+	string str("aabca");
+	string str1("aab");
+	str = str + '0' + str1;
+	cout << str << endl;
+	auto len = str.size();
+	int *suff_rank = new int[len]();
+	int *suff_array = new int[len]();
+	int *height = new int[len]();
+
+	//创建一个排名数组suff[str.size()]
+	set_suffix_rank_array(str, suff_rank, len);
+	//创建后缀数组，排第几的是谁
+	set_suffix_array(suff_rank, suff_array, len);
+	//创建高度数组
+	set_height_array(str, suff_rank, suff_array, height, len);
+
+	/*求一个字符串中最长重复子串，并输出。
+	解题思路：只需求得height中的最大值，并根据height数组中的位置反推出字符串中的位置
+	*/
+	int pos(0), lengthest = height[0];
+	for (unsigned i = 1; i < len-1; i++)
+	{
+		if (height[i] > lengthest)
+		{
+			lengthest = height[i];
+			pos = i;
+		}
+	}
+	cout <<suff_array[pos]<< " "<<lengthest << endl;
+	for (unsigned j = 0; j < lengthest; j++)
+	{
+		cout << str[suff_array[pos] + j];
+	}
+	cout << endl;
+
+	/*求最长公共子串的问题
+	 解题思路：先将两个字符串前后相连，然后求最长子串，
+	 要注意两个子串不在同一字符串中
+	*/
+
+	system("pause");
+	return 0;
+}
---
+++ 后缀数组/MakeSuffixArray.h
@@ -0,0 +1,76 @@
+﻿#include<iostream>
+#include<string>
+#include<Windows.h>
+using namespace std;
+
+//字典序比较两个字符串,iter_1 > iter_2 ->return true
+bool Cmp(string::iterator iter_1, string::iterator iter_2,string::iterator end)
+{
+	string tmp_str_1(iter_1, end);
+	string tmp_str_2(iter_2, end);
+
+	//tmp_str_1 > tmp_str_2
+	if (tmp_str_1.compare(tmp_str_2) > 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+//求出来某个后缀数组suf的排名
+int Rank(string str, unsigned suf)
+{
+	int rank = 0;
+
+	for (auto  iter = str.begin(); iter != str.end(); iter++)
+	{
+		if (iter == (str.begin() + suf))
+		{
+			continue;
+		}
+		else if ( Cmp(str.begin() + suf, iter, str.end()) )
+		{
+			rank++;
+		}
+	}
+	return rank;
+}
+
+//创建一个排名数组
+void set_suffix_rank_array(string str, int suff_rank[],unsigned len)
+{
+	for(unsigned i = 0; i < len; i++)
+	{
+		suff_rank[i] = Rank(str, i);
+	}
+}
+
+//创建后缀数组
+void set_suffix_array(int suff_rank[], int suff_array[], unsigned len)
+{
+	for (unsigned i = 0; i < len; i++)
+	{
+		suff_array[suff_rank[i]] = i;
+	}
+}
+
+//依次将排名相近的两个后缀数组从前向后计数（有相同前缀）
+void set_height_array(string str,int suff_rank[], int suff_array[],int height[], unsigned len)
+{
+	unsigned h(0),r(0);
+	for (unsigned i = 0; i < len; i++)
+	{
+		h = 0;
+		r = suff_rank[i];//只是为了表达方便
+		//其中的i表示的是suff_array[suff_rank[i]],为了书写方便
+		while ((r+ 1<len) && (i + h < len) && (suff_array[r + 1] + h < len )
+			&& (str[i + h] == str[suff_array[r + 1] + h]))
+		{
+			h++;
+		}
+		height[suff_rank[i]] = h;
+	}
+}
